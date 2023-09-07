ceab76f448efcefcc677cb84ffab0b474dfc3284
Carlos González <cgonzalez@shokworks.io>
2023-09-07 08:21:31 -0400

    wip

--- examples/register.ts
+++ examples/register.ts
@@ -15,16 +15,49 @@ function onDeleteCompletionCallback(fileId: string) {
     return true;
 }
 
-function onRenameCallback(newName: string, fileId: string) {
+async function onRenameCallback(newName: string, fileId: string): Promise<boolean> {
     console.log("Rename callback triggered.");
-    console.log("File ID: " + fileId)
-    console.log("New name: " + newName)
-    return true;
+    console.log("File ID: " + fileId);
+    console.log("New name: " + newName);
+
+    const a = await (new Promise<boolean>((resolve, reject) => {
+        try {
+
+            setTimeout(() => {
+                console.log("Inside setTimeout, resolving promise.");
+                resolve(true);
+            }, 1000)
+        } catch (err) {
+            console.log("Inside setTimeout, rejecting promise.");
+            reject(err);
+        }
+    }));
+
+    console.log("Promise resolved: ", a);
+
+    return a;
+}
+
+function onRenameCallbackWithCallback(newName: string, fileId: string, responseCallback: (response: boolean) => void) {
+    console.log("Inside onRenameCallbackWithCallback");
+    console.log("typeof responseCallback: ", typeof responseCallback);
+    setTimeout(() => responseCallback(true), 1000);
+    console.log("Inside onRenameCallbackWithCallback, after setTimeout");
+    // onRenameCallback(newName, fileId)
+    // .then(result => {
+    //     console.log("Inside .then");
+    //     console.log("Promise resolved: ", result);
+    //     responseCallback(result);
+    // }).catch(err => {
+    //     console.log("Inside .catch");
+    //     console.log("Promise rejected: ", err);
+    //     responseCallback(false);
+    // });
 }
 
 drive.connectSyncRoot({
     notifyDeleteCallback: onDeleteCompletionCallback,
-    notifyRenameCallback: onRenameCallback
+    notifyRenameCallback: onRenameCallbackWithCallback,//onRenameCallback
 });
 
 drive.createItemByPath(`/A (5th copy).pdfs`, '280ab650-acef-4438-8bbc-29863810b24a');
