71a9619839294acf4b528d05992c0e6b5cbc5291
Carlos González <cgonzalez@shokworks.io>
2024-02-08 13:36:16 -0400

    feat: include update in download callback

--- examples/get-state.ts
+++ examples/get-state.ts
@@ -1,9 +1,28 @@
 import VirtualDrive from '../src/virtual-drive';
 import settings from './settings';
+import yargs from 'yargs';
+
+// Configura yargs
+const argv = yargs
+    .command('file', 'El path del archivo para obtener el estado', {
+        path: {
+            description: 'el path del archivo',
+            alias: 'f',
+            type: 'string',
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 const drive = new VirtualDrive(settings.syncRootPath, settings.defaultLogPath);
 
-const a = drive.getPlaceholderState('\\file1.txt');
-const b = drive.getPlaceholderState('\\imagen.rar');
-console.log("file1.txt", b);
-console.log("imagen.rar", a);
+//@ts-ignore
+if (argv.file) {
+    //@ts-ignore
+    const path = argv.file;
+    const state = drive.getPlaceholderState(path);
+    console.log(`${path} state:`, state);
+} else {
+    console.log("Por favor especifica un archivo con --file <path>");
+}
