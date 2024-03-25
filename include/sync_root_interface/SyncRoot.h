0c9a9a03addb8e1ac6d4118c50019ac07370e651
Carlos González <cgonzalez@shokworks.io>
2024-03-25 10:38:27 -0400

    feat: structure refactor

--- include/sync_root_interface/SyncRoot.h
+++ include/sync_root_interface/SyncRoot.h
@@ -18,7 +18,5 @@ public:
     static HRESULT ConnectSyncRoot(const wchar_t *syncRootPath, InputSyncCallbacks syncCallbacks, napi_env env, CF_CONNECTION_KEY *connectionKey);
     static HRESULT DisconnectSyncRoot();
     static HRESULT UnregisterSyncRoot();
-    static std::list<ItemInfo> GetItemsSyncRoot(const wchar_t *syncRootPath);
-    static std::string GetFileIdentity(const wchar_t *path);
     static void DeleteFileSyncRoot(const wchar_t *path);
 };
