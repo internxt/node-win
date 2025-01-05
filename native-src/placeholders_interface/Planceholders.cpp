21a9f7cf3ab0f22f1a67af7cbef0b60ae0136d35
Carlos González <cgonzalez@shokworks.io>
2025-01-05 10:56:52 +0100

    chore: clean up

--- native-src/placeholders_interface/Planceholders.cpp
+++ native-src/placeholders_interface/Planceholders.cpp
@@ -544,8 +544,7 @@ HRESULT Placeholders::UpdatePinState(const std::wstring &path, const PinState st
 
     const auto cfState = pinStateToCfPinState(state);
     HRESULT result = CfSetPinState(handleForPath(path).get(), cfState, CF_SET_PIN_FLAG_NONE, nullptr);
-
-    // call to force shell refresh
+s
     ForceShellRefresh(path);
 
     if (result != S_OK)
