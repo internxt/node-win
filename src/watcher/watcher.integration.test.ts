719010b40a425a622c8c66214e18427a6a20b57e
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2025-01-23 16:15:57 +0100

    Update watcher.integration.test.ts

--- src/watcher/watcher.integration.test.ts
+++ src/watcher/watcher.integration.test.ts
@@ -1,33 +1,123 @@
 import { QueueManager } from "examples/queueManager";
-import { mkdir, writeFile } from "fs/promises";
+import { existsSync } from "fs";
+import { mkdir, rmdir, writeFile } from "fs/promises";
+import { join } from "path";
 import { TEST_FILES } from "test/utils/setup.helper.test";
+import { sleep } from "test/utils/sleep.helper.test";
 import { v4 } from "uuid";
 import { beforeEach } from "vitest";
 import { mockDeep } from "vitest-mock-extended";
 
+import { OnAddDirService } from "./events/on-add-dir.service";
+import { OnAddService } from "./events/on-add.service";
+import { OnRawService } from "./events/on-raw.service";
 import { Watcher } from "./watcher";
 import { IVirtualDriveFunctions } from "./watcher.interface";
 
 describe("Watcher", () => {
   const virtualDriveFn = mockDeep<IVirtualDriveFunctions>();
   const queueManager = mockDeep<QueueManager>();
-  const syncRootPath = `${TEST_FILES}/${v4()}`;
-  const logPath = `${TEST_FILES}/${v4()}.log`;
+  const syncRootPath = join(TEST_FILES, v4());
+  const logPath = join(TEST_FILES, `${v4()}.log`);
   const options = {};
-  console.log("🚀 ~ describe ~ syncRootPath:", syncRootPath);
 
-  const watcher = new Watcher();
+  const onAdd = mockDeep<OnAddService>();
+  const onAddDir = mockDeep<OnAddDirService>();
+  const onRaw = mockDeep<OnRawService>();
+
+  const watcher = new Watcher(onAdd, onAddDir);
   watcher.init(queueManager, syncRootPath, options, logPath, virtualDriveFn);
 
   beforeEach(async () => {
+    vi.clearAllMocks();
+
+    if (existsSync(syncRootPath)) {
+      await rmdir(syncRootPath, { recursive: true });
+    }
     await mkdir(syncRootPath);
   });
 
-  it("", async () => {
-    // Arrange
-    watcher.watchAndWait();
+  describe("When call watchAndWait", () => {
+    it("When folder is empty, then emit one addDir event", async () => {
+      // Act
+      watcher.watchAndWait();
+      await sleep(50);
 
-    // Act
-    await writeFile(`${syncRootPath}/watcher-integration.txt`, Buffer.alloc(1000));
+      // Assert
+      expect(onAdd.execute).toHaveBeenCalledTimes(0);
+      expect(onAddDir.execute).toHaveBeenCalledTimes(1);
+      expect(onRaw.execute).toHaveBeenCalledTimes(0);
+      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: syncRootPath }));
+    });
+
+    it("When folder has one file, then emit one addDir and one add event", async () => {
+      // Arrange
+      const file = join(syncRootPath, `${v4()}.txt`);
+      await writeFile(file, Buffer.alloc(1000));
+
+      // Act
+      watcher.watchAndWait();
+      await sleep(50);
+
+      // Assert
+      expect(onAdd.execute).toHaveBeenCalledTimes(1);
+      expect(onAddDir.execute).toHaveBeenCalledTimes(1);
+      expect(onRaw.execute).toHaveBeenCalledTimes(0);
+      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
+      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: syncRootPath }));
+    });
   });
+
+  describe("When add items", () => {
+    it("When add an empty folder, then emit one addDir event", async () => {
+      // Arrange
+      const folder = join(syncRootPath, v4());
+      watcher.watchAndWait();
+
+      // Act
+      await mkdir(folder);
+      await sleep(50);
+
+      // Assert
+      expect(onAdd.execute).toHaveBeenCalledTimes(0);
+      expect(onAddDir.execute).toHaveBeenCalledTimes(2);
+      expect(onRaw.execute).toHaveBeenCalledTimes(0);
+      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: folder }));
+    });
+
+    it("When add a file, then emit one add event", async () => {
+      // Arrange
+      const file = join(syncRootPath, `${v4()}.txt`);
+      watcher.watchAndWait();
+
+      // Act
+      await writeFile(file, Buffer.alloc(1000));
+      await sleep(50);
+
+      // Assert
+      expect(onAdd.execute).toHaveBeenCalledTimes(1);
+      expect(onAddDir.execute).toHaveBeenCalledTimes(1);
+      expect(onRaw.execute).toHaveBeenCalledTimes(0);
+      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
+    });
+
+    it("When add a folder and a file inside, then emit one addDir and one add events", async () => {
+      // Arrange
+      const folder = join(syncRootPath, v4());
+      const file = join(folder, `${v4()}.txt`);
+      watcher.watchAndWait();
+
+      // Act
+      await mkdir(folder);
+      await writeFile(file, Buffer.alloc(1000));
+      await sleep(50);
+
+      // Assert
+      expect(onAdd.execute).toHaveBeenCalledTimes(1);
+      expect(onAddDir.execute).toHaveBeenCalledTimes(2);
+      expect(onRaw.execute).toHaveBeenCalledTimes(0);
+      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
+      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: folder }));
+    });
+  })
 });
