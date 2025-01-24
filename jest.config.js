f78eccb01dc079ec6ac1df8f0f345739bf2166d2
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2025-01-24 14:06:31 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.e2e.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ["<rootDir>/test/utils/setup.helper.test.ts"],
    moduleNameMapper: {
     "^examples/(.*)$": "<rootDir>/examples/$1",
+    "^test/(.*)$": "<rootDir>/test/$1",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   coverageThreshold: {
