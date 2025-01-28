4912a9867676bd595505fecf7f5b1ee6aa2b6c47
Daniel Jiménez Rivera <dani@internxt.com>
2025-01-28 16:18:05 +0100

    Update on-add.service.ts

--- src/watcher/events/on-add.service.ts
+++ src/watcher/events/on-add.service.ts
@@ -4,22 +4,28 @@ import { typeQueue } from "@/queue/queueManager";
 import { PinState, SyncState } from "@/types/placeholder.type";
 
 import { Watcher } from "../watcher";
-import { logger } from "@/logger";
-import { extname } from "path";
 
 export class OnAddService {
   execute({ self, path, stats }: TProps) {
     try {
-      if (extname(path) === "") return;
+      self.writeLog("onAdd", path, stats);
+      const ext = path.split(".").pop();
 
       const { size, birthtime, mtime } = stats;
-      if (size === 0 || size > 20 * 1024 * 1024 * 1024) return;
 
       const fileIntenty = self.virtualDriveFn.CfGetPlaceHolderIdentity(path);
-      const status = self.virtualDriveFn.CfGetPlaceHolderState(path);
 
+      self.writeLog("fileIntenty in add", fileIntenty);
+
+      if (!ext || size === 0 || size > 20 * 1024 * 1024 * 1024) return;
+
+      const status = self.virtualDriveFn.CfGetPlaceHolderState(path);
+      self.writeLog("status", status);
+
+      // Verificar tiempos de creación y modificación
       const creationTime = new Date(birthtime).getTime();
       const modificationTime = new Date(mtime).getTime();
+      const currentTime = Date.now();
 
       let isNewFile = false;
       let isMovedFile = false;
@@ -32,18 +38,6 @@ export class OnAddService {
         isMovedFile = true;
       }
 
-      self.writeLog({
-        event: "onAdd",
-        path,
-        fileIntenty,
-        size,
-        birthtime,
-        mtime,
-        status,
-        isNewFile,
-        isMovedFile,
-      });
-
       // Procesar el archivo según su estado
       if (status.pinState === PinState.AlwaysLocal || status.pinState === PinState.OnlineOnly || status.syncState === SyncState.InSync) {
         return;
@@ -53,10 +47,12 @@ export class OnAddService {
         self.fileInDevice.add(path);
         self.queueManager.enqueue({ path, type: typeQueue.add, isFolder: false });
       } else if (isMovedFile) {
+        self.writeLog("File moved:", path);
         // Procesar archivo movido según sea necesario
       }
     } catch (error) {
-      logger.error(error, "onAdd");
+      console.log("Error en onAdd");
+      console.error(error);
     }
   }
 }
