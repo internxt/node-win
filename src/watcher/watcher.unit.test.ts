490003c0bb7dafa2de167435971ad0b5bb268116
Daniel Jiménez Rivera <dani@internxt.com>
2025-02-08 13:03:25 +0100

    Fix watcher tests

--- src/watcher/watcher.unit.test.ts
+++ src/watcher/watcher.unit.test.ts
@@ -14,17 +14,18 @@ import { sleep } from "@/utils";
 
 import { OnAddDirService } from "./events/on-add-dir.service";
 import { OnAddService } from "./events/on-add.service";
-import { OnAllService } from "./events/on-all.service";
 import { OnRawService } from "./events/on-raw.service";
 import { Watcher } from "./watcher";
 
 describe("Watcher", () => {
+  let watcher: Watcher | undefined;
+
   const addon = mockDeep<Addon>();
   const queueManager = mockDeep<QueueManager>();
   const logger = mockDeep<Logger>();
   const options = {};
 
-  const onAll = mockDeep<OnAllService>();
+  const onAll = vi.fn();
   const onAdd = mockDeep<OnAddService>();
   const onAddDir = mockDeep<OnAddDirService>();
   const onRaw = mockDeep<OnRawService>();
@@ -34,17 +35,22 @@ describe("Watcher", () => {
       await mkdir(syncRootPath);
     }
 
-    const watcher = new Watcher(onAll, onAdd, onAddDir, onRaw);
+    watcher = new Watcher(onAdd, onAddDir, onRaw);
     watcher.init(queueManager, syncRootPath, options, logger, addon);
     watcher.watchAndWait();
+    watcher.chokidar?.on("all", (event, path) => onAll({ event, path }));
   };
 
   const getEvents = () => {
-    return onAll.execute.mock.calls.map((call) => call[0].event);
+    return onAll.mock.calls.map((call) => ({ event: call[0].event, path: call[0].path }));
   };
 
-  beforeEach(async () => {
+  beforeEach(() => {
     vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    watcher?.chokidar?.close();
   });
 
   describe("[Watcher] When call watchAndWait", () => {
@@ -54,29 +60,33 @@ describe("Watcher", () => {
       await mkdir(syncRootPath);
 
       // Act
+      await sleep(50);
       await setupWatcher(syncRootPath);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir"]);
-      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: syncRootPath }));
+      expect(getEvents()).toStrictEqual([{ event: "addDir", path: syncRootPath }]);
     });
 
     it("When folder has one file, then emit one addDir and one add event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const file = join(syncRootPath, `${v4()}.txt`);
+      const file = join(syncRootPath, v4());
       await mkdir(syncRootPath);
       await writeFile(file, Buffer.alloc(1000));
 
       // Act
+      await sleep(50);
       await setupWatcher(syncRootPath);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add"]);
-      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
-      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: syncRootPath }));
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+        ]),
+      );
     });
   });
 
@@ -88,60 +98,80 @@ describe("Watcher", () => {
       await setupWatcher(syncRootPath);
 
       // Act
+      await sleep(50);
       await mkdir(folder);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir"]);
-      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: folder }));
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+        ]),
+      );
     });
 
     it("When add a file, then emit one add event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const file = join(syncRootPath, `${v4()}.txt`);
+      const file = join(syncRootPath, v4());
       await setupWatcher(syncRootPath);
 
       // Act
+      await sleep(50);
       await writeFile(file, Buffer.alloc(1000));
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add"]);
-      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+        ]),
+      );
     });
 
     it("When add a file of zero size, then emit one add event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const file = join(syncRootPath, `${v4()}.txt`);
+      const file = join(syncRootPath, v4());
       await setupWatcher(syncRootPath);
 
       // Act
+      await sleep(50);
       await writeFile(file, Buffer.alloc(0));
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add"]);
-      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+        ]),
+      );
     });
 
     it("When add a folder and a file inside, then emit one addDir and one add event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
       const folder = join(syncRootPath, v4());
-      const file = join(folder, `${v4()}.txt`);
+      const file = join(folder, v4());
       await setupWatcher(syncRootPath);
 
       // Act
+      await sleep(50);
       await mkdir(folder);
       await writeFile(file, Buffer.alloc(1000));
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir", "add"]);
-      expect(onAdd.execute).toHaveBeenCalledWith(expect.objectContaining({ path: file }));
-      expect(onAddDir.execute).toHaveBeenCalledWith(expect.objectContaining({ path: folder }));
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+          { event: "add", path: file },
+        ]),
+      );
     });
   });
 
@@ -149,27 +179,23 @@ describe("Watcher", () => {
     it("When modify a file, then emit one change event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const fileName = `${v4()}.txt`;
+      const fileName = v4();
       const file = join(syncRootPath, fileName);
       await setupWatcher(syncRootPath);
       await writeFile(file, Buffer.alloc(1000));
-      
+
       // Act
       await sleep(50);
       await appendFile(file, Buffer.alloc(1000));
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add", "change"]);
-      expect(onRaw.execute).toHaveBeenCalledWith(
-        expect.objectContaining({
-          event: "change",
-          path: fileName,
-          details: {
-            watchedPath: file,
-            // TODO: why does not include prev and curr stats
-          },
-        }),
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+          { event: "change", path: file },
+        ]),
       );
     });
   });
@@ -178,19 +204,26 @@ describe("Watcher", () => {
     it("When rename a file, then do not emit any event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const fileName1 = `${v4()}.txt`;
-      const fileName2 = `${v4()}.txt`;
+      const fileName1 = v4();
+      const fileName2 = v4();
       const file1 = join(syncRootPath, fileName1);
       const file2 = join(syncRootPath, fileName2);
       await setupWatcher(syncRootPath);
       await writeFile(file1, Buffer.alloc(1000));
 
       // Act
+      await sleep(50);
       await rename(file1, file2);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file1 },
+          { event: "add", path: file2 },
+        ]),
+      );
     });
 
     it("When rename a folder, then do not emit any event", async () => {
@@ -202,11 +235,19 @@ describe("Watcher", () => {
       await mkdir(folder1);
 
       // Act
+      await sleep(50);
       await rename(folder1, folder2);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder1 },
+          { event: "unlinkDir", path: folder1 },
+          { event: "addDir", path: folder2 },
+        ]),
+      );
     });
   });
 
@@ -215,21 +256,30 @@ describe("Watcher", () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
       const folder = join(syncRootPath, v4());
-      const fileName = `${v4()}.txt`;
+      const fileName = v4();
       const file = join(syncRootPath, fileName);
+      const movedFile = join(folder, fileName);
       await setupWatcher(syncRootPath);
       await mkdir(folder);
       await writeFile(file, Buffer.alloc(1000));
 
       // Act
-      await rename(file, join(folder, fileName));
+      await sleep(50);
+      await rename(file, movedFile);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir", "add"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+          { event: "addDir", path: folder },
+          { event: "add", path: movedFile },
+        ]),
+      );
     });
 
-    it("When move a folder to a folder, then do not emit any event", async () => {
+    it("When move a folder to a folder, then emit one unlinkDir and one addDir event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
       const folder = join(syncRootPath, v4());
@@ -237,15 +287,24 @@ describe("Watcher", () => {
       const folder1 = join(syncRootPath, folderName);
       const folder2 = join(folder, folderName);
       await setupWatcher(syncRootPath);
-
-      // Act
       await mkdir(folder);
       await mkdir(folder1);
+
+      // Act
+      await sleep(50);
       await rename(folder1, folder2);
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir", "addDir"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+          { event: "addDir", path: folder1 },
+          { event: "unlinkDir", path: folder1 },
+          { event: "addDir", path: folder2 },
+        ]),
+      );
     });
   });
 
@@ -253,7 +312,7 @@ describe("Watcher", () => {
     it("When delete a file, then emit one unlink event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const file = join(syncRootPath, `${v4()}.txt`);
+      const file = join(syncRootPath, v4());
       await setupWatcher(syncRootPath);
       await writeFile(file, Buffer.alloc(1000));
 
@@ -263,7 +322,13 @@ describe("Watcher", () => {
       await sleep(150);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add", "unlink"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+          { event: "unlink", path: file },
+        ]),
+      );
     });
 
     it("When delete a folder, then emit one unlinkDir event", async () => {
@@ -279,7 +344,13 @@ describe("Watcher", () => {
       await sleep(150);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir", "unlinkDir"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+          { event: "unlinkDir", path: folder },
+        ]),
+      );
     });
   });
 
@@ -287,7 +358,7 @@ describe("Watcher", () => {
     it("When pin a file, then emit one change event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const fileName = `${v4()}.txt`;
+      const fileName = v4();
       const file = join(syncRootPath, fileName);
       await setupWatcher(syncRootPath);
       await writeFile(file, Buffer.alloc(1000));
@@ -298,16 +369,12 @@ describe("Watcher", () => {
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add", "change"]);
-      expect(onRaw.execute).toHaveBeenCalledWith(
-        expect.objectContaining({
-          event: "change",
-          path: fileName,
-          details: {
-            watchedPath: file,
-            // TODO: why does not include prev and curr stats
-          },
-        }),
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+          { event: "change", path: file },
+        ]),
       );
     });
 
@@ -324,7 +391,12 @@ describe("Watcher", () => {
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+        ]),
+      );
     });
   });
 
@@ -332,7 +404,7 @@ describe("Watcher", () => {
     it("When unpin a file, then emit one change event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const fileName = `${v4()}.txt`;
+      const fileName = v4();
       const file = join(syncRootPath, fileName);
       await setupWatcher(syncRootPath);
       await writeFile(file, Buffer.alloc(1000));
@@ -345,16 +417,13 @@ describe("Watcher", () => {
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add", "change", "change"]);
-      expect(onRaw.execute).toHaveBeenCalledWith(
-        expect.objectContaining({
-          event: "change",
-          path: fileName,
-          details: {
-            watchedPath: file,
-            // TODO: why does not include prev and curr stats
-          },
-        }),
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+          { event: "change", path: file },
+          { event: "change", path: file },
+        ]),
       );
     });
 
@@ -373,7 +442,12 @@ describe("Watcher", () => {
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+        ]),
+      );
     });
   });
 
@@ -381,7 +455,7 @@ describe("Watcher", () => {
     it("When set a file to online only, then emit one change event", async () => {
       // Arrange
       const syncRootPath = join(TEST_FILES, v4());
-      const fileName = `${v4()}.txt`;
+      const fileName = v4();
       const file = join(syncRootPath, fileName);
       await setupWatcher(syncRootPath);
       await writeFile(file, Buffer.alloc(1000));
@@ -392,16 +466,12 @@ describe("Watcher", () => {
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "add", "change"]);
-      expect(onRaw.execute).toHaveBeenCalledWith(
-        expect.objectContaining({
-          event: "change",
-          path: fileName,
-          details: {
-            watchedPath: file,
-            // TODO: why does not include prev and curr stats
-          },
-        }),
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "add", path: file },
+          { event: "change", path: file },
+        ]),
       );
     });
 
@@ -418,7 +488,12 @@ describe("Watcher", () => {
       await sleep(50);
 
       // Assert
-      expect(getEvents()).toEqual(["addDir", "addDir"]);
+      expect(getEvents()).toStrictEqual(
+        expect.arrayContaining([
+          { event: "addDir", path: syncRootPath },
+          { event: "addDir", path: folder },
+        ]),
+      );
     });
   });
 });
