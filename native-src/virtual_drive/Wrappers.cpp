07162b45f80c31ac4cba2b9f03f7153523eaf495
Daniel Jiménez Rivera <dani@internxt.com>
2025-09-09 18:37:47 +0200

    Extract get file identity (#185)
    * Create napi safe wrap
    
    * Extract create folder placeholder
    
    * Extract create file placeholder
    
    * Extract get file identity
    
    * Update package.json

--- native-src/virtual_drive/Wrappers.cpp
+++ native-src/virtual_drive/Wrappers.cpp
@@ -11,6 +11,7 @@
 #include "register_sync_root.h"
 #include "create_folder_placeholder.h"
 #include "create_file_placeholder.h"
+#include "get_file_identity.h"
 #include "napi_safe_wrap.h"
 
 std::string WStringToUTF8(const std::wstring &wstr)
@@ -271,31 +272,7 @@ napi_value DisconnectSyncRootWrapper(napi_env env, napi_callback_info args)
 
 napi_value GetFileIdentityWrapper(napi_env env, napi_callback_info args)
 {
-    size_t argc = 1;
-    napi_value argv[1];
-    napi_get_cb_info(env, args, &argc, argv, nullptr, nullptr);
-
-    if (argc < 1)
-    {
-        napi_throw_error(env, nullptr, "The path is required for GetFileIdentity");
-        return nullptr;
-    }
-
-    LPCWSTR fullPath;
-    size_t pathLength;
-    napi_get_value_string_utf16(env, argv[0], nullptr, 0, &pathLength);
-    fullPath = new WCHAR[pathLength + 1];
-    napi_get_value_string_utf16(env, argv[0], reinterpret_cast<char16_t *>(const_cast<wchar_t *>(fullPath)), pathLength + 1, nullptr);
-
-    std::string fileIdentity = Placeholders::GetFileIdentity(fullPath);
-    fileIdentity.erase(std::remove(fileIdentity.begin(), fileIdentity.end(), '\0'), fileIdentity.end());
-    fileIdentity.erase(std::remove(fileIdentity.begin(), fileIdentity.end(), ' '), fileIdentity.end());
-
-    napi_value jsFileIdentity;
-    napi_create_string_utf8(env, fileIdentity.c_str(), fileIdentity.length(), &jsFileIdentity);
-
-    delete[] fullPath;
-    return jsFileIdentity;
+    return napi_safe_wrap(env, args, get_file_identity_impl);
 }
 
 napi_value DeleteFileSyncRootWrapper(napi_env env, napi_callback_info args)
