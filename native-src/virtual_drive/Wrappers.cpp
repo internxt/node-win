ce01cdc964dc9706196785280959e51d514a7c92
Daniel Jiménez Rivera <dani@internxt.com>
2025-09-15 17:03:35 +0200

    Extract hydrate file (#187)
    * Create napi safe wrap
    
    * Extract create folder placeholder
    
    * Extract create file placeholder
    
    * Extract get file identity
    
    * Update package.json
    
    * Extract connect sync root
    
    * Extract hydrate file

--- native-src/virtual_drive/Wrappers.cpp
+++ native-src/virtual_drive/Wrappers.cpp
@@ -13,6 +13,7 @@
 #include "create_file_placeholder.h"
 #include "get_file_identity.h"
 #include "connect_sync_root.h"
+#include "hydrate_file.h"
 #include "napi_safe_wrap.h"
 
 std::string WStringToUTF8(const std::wstring &wstr)
@@ -408,50 +409,7 @@ napi_value UpdateFileIdentityWrapper(napi_env env, napi_callback_info args)
 
 napi_value HydrateFileWrapper(napi_env env, napi_callback_info args)
 {
-    size_t argc = 1;
-    napi_value argv[1];
-    napi_value thisArg;
-    napi_get_cb_info(env, args, &argc, argv, &thisArg, nullptr);
-
-    if (argc < 1)
-    {
-        napi_throw_type_error(env, nullptr, "The file path is required for HydrateFile");
-        return nullptr;
-    }
-
-    // Obtener el argumento de JavaScript y convertirlo a una cadena de C++
-    size_t pathLength;
-    napi_get_value_string_utf16(env, argv[0], nullptr, 0, &pathLength);
-    std::wstring fullPath(pathLength, L'\0');
-    napi_get_value_string_utf16(env, argv[0], reinterpret_cast<char16_t *>(&fullPath[0]), pathLength + 1, nullptr);
-
-    // Crear una promesa
-    napi_deferred deferred;
-    napi_value promise;
-    napi_create_promise(env, &deferred, &promise);
-
-    // Lanzar la operación asíncrona en un hilo separado
-    std::thread([deferred, fullPath, env]()
-                {
-        try {
-            SyncRoot::HydrateFile(fullPath.c_str());
-             Logger::getInstance().log("finish... " + Logger::fromWStringToString(fullPath.c_str()), LogLevel::INFO);
-
-            napi_value result;
-            napi_get_undefined(env, &result);
-            napi_resolve_deferred(env, deferred, result);
-        } catch (const std::exception& e) {
-            napi_value error;
-            napi_create_string_utf8(env, e.what(), NAPI_AUTO_LENGTH, &error);
-            napi_reject_deferred(env, deferred, error);
-        } catch (...) {
-            napi_value error;
-            napi_create_string_utf8(env, "Unknown error", NAPI_AUTO_LENGTH, &error);
-            napi_reject_deferred(env, deferred, error);
-        } })
-        .detach();
-
-    return promise;
+    return napi_safe_wrap(env, args, hydrate_file_impl);
 }
 
 napi_value DehydrateFileWrapper(napi_env env, napi_callback_info args)
