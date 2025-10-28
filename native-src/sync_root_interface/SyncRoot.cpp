3dceab3a7156e650af46690105538464943f4dc6
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-28 23:38:37 +0100

    Commit

--- native-src/sync_root_interface/SyncRoot.cpp
+++ native-src/sync_root_interface/SyncRoot.cpp
@@ -8,51 +8,6 @@
 
 std::map<std::wstring, CF_CONNECTION_KEY> connectionMap;
 
-void SyncRoot::HydrateFile(const wchar_t *filePath)
-{
-    wprintf(L"Hydration file started %ls\n", filePath);
-    DWORD attrib = GetFileAttributesW(filePath);
-    if (!(attrib & FILE_ATTRIBUTE_DIRECTORY))
-    {
-        winrt::handle placeholder(CreateFileW(filePath, 0, FILE_READ_DATA, nullptr, OPEN_EXISTING, 0, nullptr));
-
-        LARGE_INTEGER offset;
-        offset.QuadPart = 0;
-        LARGE_INTEGER length;
-        GetFileSizeEx(placeholder.get(), &length);
-
-        if (attrib & FILE_ATTRIBUTE_PINNED)
-        {
-            // if (!(attrib & FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS))
-            // {
-            Logger::getInstance().log("Hydration file init", LogLevel::INFO);
-
-            auto start = std::chrono::steady_clock::now();
-
-            HRESULT hr = CfHydratePlaceholder(placeholder.get(), offset, length, CF_HYDRATE_FLAG_NONE, NULL);
-
-            if (FAILED(hr))
-            {
-                Logger::getInstance().log("Error hydrating file " + Logger::fromWStringToString(filePath), LogLevel::ERROR);
-            }
-            else
-            {
-                auto end = std::chrono::steady_clock::now();
-                auto elapsedMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-
-                if (elapsedMilliseconds < 200)
-                {
-                    wprintf(L"Already Hydrated: %d ms\n", elapsedMilliseconds);
-                }
-                else
-                {
-                    wprintf(L"Hydration finished %ls\n", filePath);
-                }
-            }
-        }
-    }
-}
-
 HRESULT SyncRoot::ConnectSyncRoot(const wchar_t *syncRootPath, InputSyncCallbacks syncCallbacks, napi_env env, CF_CONNECTION_KEY *connectionKey)
 {
     register_threadsafe_fetch_data_callback("FetchDataThreadSafe", env, syncCallbacks);
@@ -61,23 +16,22 @@ HRESULT SyncRoot::ConnectSyncRoot(const wchar_t *syncRootPath, InputSyncCallback
     CF_CALLBACK_REGISTRATION callbackTable[] = {
         {CF_CALLBACK_TYPE_FETCH_DATA, fetch_data_callback_wrapper},
         {CF_CALLBACK_TYPE_CANCEL_FETCH_DATA, cancel_fetch_data_callback_wrapper},
-        CF_CALLBACK_REGISTRATION_END
-    };
+        CF_CALLBACK_REGISTRATION_END};
 
     HRESULT hr = CfConnectSyncRoot(
         syncRootPath,
         callbackTable,
         nullptr,
         CF_CONNECT_FLAG_REQUIRE_PROCESS_INFO | CF_CONNECT_FLAG_REQUIRE_FULL_FILE_PATH,
-        connectionKey
-    );
+        connectionKey);
 
     wprintf(L"Connection key: %llu\n", connectionKey->Internal);
-    
-    if (SUCCEEDED(hr)) {
+
+    if (SUCCEEDED(hr))
+    {
         connectionMap[syncRootPath] = *connectionKey;
     }
-    
+
     return hr;
 }
 
--- native-src/virtual_drive/dehydrate_file.cpp
+++ native-src/virtual_drive/dehydrate_file.cpp
@@ -4,17 +4,16 @@
 
 napi_value dehydrate_file(napi_env env, napi_callback_info info)
 {
-    auto [rawPath] = napi_extract_args<std::wstring>(env, info);
-    const wchar_t *path = rawPath.c_str();
+    auto [path] = napi_extract_args<std::wstring>(env, info);
 
-    DWORD attrib = GetFileAttributesW(path);
+    DWORD attrib = GetFileAttributesW(path.c_str());
 
     if (attrib & FILE_ATTRIBUTE_DIRECTORY)
     {
         throw std::runtime_error("Cannot dehydrate folder");
     }
 
-    winrt::handle fileHandle(CreateFileW(path, 0, FILE_READ_DATA, nullptr, OPEN_EXISTING, 0, nullptr));
+    winrt::handle fileHandle(CreateFileW(path.c_str(), 0, FILE_READ_DATA, nullptr, OPEN_EXISTING, 0, nullptr));
 
     LARGE_INTEGER offset;
     offset.QuadPart = 0;
