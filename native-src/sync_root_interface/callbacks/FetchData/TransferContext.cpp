69e9f82ceb7c645bc54f370ad642d16d3163d58f
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-05 11:50:52 +0100

    Commit

--- native-src/sync_root_interface/callbacks/FetchData/TransferContext.cpp
+++ native-src/sync_root_interface/callbacks/FetchData/TransferContext.cpp
@@ -14,7 +14,7 @@ static std::mutex g_contextMapMutex;
 
 std::shared_ptr<TransferContext> CreateTransferContext(CF_TRANSFER_KEY transferKey)
 {
-    std::lock_guard<std::mutex> lock(g_contextMapMutex);
+    std::scoped_lock lock(g_contextMapMutex);
 
     auto ctx = std::make_shared<TransferContext>();
     ctx->transferKey = transferKey;
@@ -25,10 +25,9 @@ std::shared_ptr<TransferContext> CreateTransferContext(CF_TRANSFER_KEY transferK
 
 std::shared_ptr<TransferContext> GetTransferContext(CF_TRANSFER_KEY transferKey)
 {
-    std::lock_guard<std::mutex> lock(g_contextMapMutex);
+    std::scoped_lock lock(g_contextMapMutex);
 
-    auto it = g_transferContextMap.find(transferKey);
-    if (it != g_transferContextMap.end())
+    if (auto it = g_transferContextMap.find(transferKey); it != g_transferContextMap.end())
     {
         return it->second;
     }
@@ -38,6 +37,6 @@ std::shared_ptr<TransferContext> GetTransferContext(CF_TRANSFER_KEY transferKey)
 
 void RemoveTransferContext(CF_TRANSFER_KEY transferKey)
 {
-    std::lock_guard<std::mutex> lock(g_contextMapMutex);
+    std::scoped_lock lock(g_contextMapMutex);
     g_transferContextMap.erase(transferKey);
 }
